7f14bcf5151d98976d952de136386bf81a27393c
Mohammed Danis <112960178+Danis2311@users.noreply.github.com>
2024-12-12 12:26:57 +0530

    business profile page app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:business/bussiness_profile_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const BusinessProfilePage(),
+      );
+}
